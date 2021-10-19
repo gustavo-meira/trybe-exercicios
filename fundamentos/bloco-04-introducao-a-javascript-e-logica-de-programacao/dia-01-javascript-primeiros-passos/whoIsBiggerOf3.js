768426dafc3d644716abb9edb64dd1e238710893
Gustavo Meira <gmeira.antonio@gmail.com>
2021-10-18 23:11:48 -0300

    Exercicio 03 - Bloco 04 - Dia 01

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/whoIsBiggerOf3.js
@@ -0,0 +1,11 @@
+const a = 2;
+const b = 7;
+const c = 4;
+
+if (a > b && a > c) {
+  console.log('A é o maior numero dos três');
+} else if (b > a && b > c) {
+  console.log('B é o maior numero dos três');
+} else if (c > a && c > b) {
+  console.log('C é o maior numero dos três');
+}
