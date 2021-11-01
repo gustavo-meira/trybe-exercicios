1603cddcbcf6b174a11b3c9568e29914de507c07
Gustavo Meira <gmeira.antonio@gmail.com>
2021-11-01 17:30:13 -0300

    [Fixing] Exercicio 01 && 02 - Bloco 04 - Dia 03

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-progamacao-e-algoritmos/exercise1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-progamacao-e-algoritmos/exercise1.js
@@ -1,6 +1,6 @@
 const numberOfAsterisk = 5;
 
-for (let line = 0; line < 5; line += 1) {
+for (let line = 0; line < numberOfAsterisk; line += 1) {
   let currLine = '';
   for (let column = 0; column < numberOfAsterisk; column += 1) {
     currLine += '*';
