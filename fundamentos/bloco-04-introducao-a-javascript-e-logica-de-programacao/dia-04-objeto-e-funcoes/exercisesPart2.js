c331853a23595bf7af3e42ab784e807d526b0193
Gustavo Meira <gmeira.antonio@gmail.com>
2021-11-01 18:28:45 -0300

    Exercicio 09 - Bloco 04 - Dia 04

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objeto-e-funcoes/exercisesPart2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objeto-e-funcoes/exercisesPart2.js
@@ -39,3 +39,17 @@ function smallerNumber(array) {
 }
 
 console.log(smallerNumber([2, 4, 6, 7, 10, 0, -3]));
+
+function biggestName(array) {
+  let biggest = array[0];
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i].length > biggest.length) {
+      biggest = array[i];
+    }
+  }
+
+  return biggest;
+}
+
+console.log(biggestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
